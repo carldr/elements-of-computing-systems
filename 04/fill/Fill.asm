db053dd6fb9932dac559e62c20d564472ac819fe
Carl Drinkwater <carl@29degrees.co.uk>
2013-06-25 00:34:31 +0100

    Add Fill.asm solution.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,56 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+
+(POLLKEYBOARD)
+	@0
+	D=A
+	@color
+	M=D
+	
+	@KBD
+	D=M
+	@SKIPBLACK
+	D;JGT
+	
+	//  Set @color to black (!0 = 65535)
+	@0
+	D=A
+	@color
+	M=!D
+		
+(SKIPBLACK)
+	@FILLSCREEN
+	D;JMP
+	
+(FILLSCREENEND)
+	@POLLKEYBOARD
+	D;JMP
+
+
+(FILLSCREEN)
+	@SCREEN
+	D=A
+	@count
+	M=D
+	
+// 32 words x 256 rows = 8192 bytes + Base screen (16384) = 24576
+(FILLSCREENLOOP)
+	@count
+	D=M
+	@24576
+	D=D-A
+	@FILLSCREENEND
+	D;JEQ
+	
+	@color
+	D=M
+	@count
+	A=M
+	M=!D
+	
+	@count
+	M=M+1
+	
+	@FILLSCREENLOOP
+	D;JMP
